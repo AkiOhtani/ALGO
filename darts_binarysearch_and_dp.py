a1fa3188dced2f87ca3694284d6a2c9cb8fe2235
Akiyoshi Otani <ohtani1116@gmail.com>
2019-08-13 16:15:35 +0900

    add darts_binary_search_and_dp.py

---
+++ darts_binarysearch_and_dp.py
@@ -0,0 +1,30 @@
+from bisect import bisect_left
+
+N, M = map(int, input().split())
+
+P = [0]
+for i in range(N):
+    P.append(int(input()))
+
+P.sort()
+
+combP = set()
+for numi in P:
+    for numj in P:
+        if (numi + numj) not in combP:
+            combP.add(numi + numj)
+
+combP = sorted(list(combP))
+
+count = [0]
+
+for numi in combP:
+    num = M - numi
+    if num >= 0:
+        index = bisect_left(combP, num)
+        if index == 0 or (index < len(P) and P[index] == num):
+            count.append(numi + combP[index])
+        else:
+            count.append(numi + combP[index-1])
+
+print(max(count))
