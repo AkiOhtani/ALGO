8a97285c207abdf7ec57175e17c5fe218ede3ada
Akiyoshi Ohtani <ohtani1116@gmail.com>
2020-06-09 21:06:15 +0900

    fixed maximal_square.py

--- maximal_square.py
+++ maximal_square.py
@@ -9,12 +9,12 @@ class Solution:
         
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
-                dp[i+1].append(int(matrix[i-1][j]))
+                dp[i+1].append(int(matrix[i][j]))
         
-        for i in range(len(matrix)+1):
-            for j in range(len(matrix[0])+1):
+        for i in range(1, len(matrix)+1):
+            for j in range(1, len(matrix[0])+1):
                 if dp[i][j] == 1:
-                    dp[i][j] = min(dp[i][j], min(dp[i-1][j-1], min(dp[i-1][j], dp[i][j-1]))) + 1
+                    dp[i][j] = dp[i][j] + min(dp[i-1][j-1], min(dp[i-1][j], dp[i][j-1]))
                     res = max(res, dp[i][j])
-        return res
+        return res**2
                 
