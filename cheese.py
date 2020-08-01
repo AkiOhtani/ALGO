5c303ac23d5d2ba51ec5db156e74f50449fe6f2e
Akiyoshi Ohtani <ohtani1116@gmail.com>
2020-08-01 18:07:00 +0900

    Update cheese.py(submitted but TLE)

--- cheese.py
+++ cheese.py
@@ -7,23 +7,28 @@ for i in range(H):
     matrix.append(list(input()))
     for j in range(W):
         if matrix[i][j] == 'S':
-            que = deque([(0, i, j, 1, set())])
-    
+            que = deque([(0, i, j, 1)])
+
+visited = set()
+
 while que:
-    depth, row, col, vital, visited = que.popleft()
-    if str(vital) < matrix[row][col] and '0' < matrix[row][col] <= '9': 
+    depth, row, col, vital = que.popleft()
+    if (row, col) in visited:
         continue
-    
-    if '.' < matrix[row][col] <= str(vital):
-        if (row, col) not in visited:
-            vital += 1
-            visited.add((row, col))
-    if matrix[row][col] == str(N):
-        print(depth)
-        exit() 
+    visited.add((row, col))
+    if str(vital) < matrix[row][col] <= '9': 
+        continue
+    elif matrix[row][col] == str(vital):
+        if matrix[row][col] == str(N):
+            print(depth)
+            exit() 
+        else:
+            matrix[row][col] = '.'
+            que = deque([(depth, row, col, vital+1)])
+            visited = set()
+            continue
 
     for ud, lr in ((-1, 0), (1, 0), (0, -1), (0, 1)):
         if not(0 <= row + ud < H and 0 <= col + lr < W and matrix[row+ud][col+lr] != 'X'):
             continue
-        que.append((depth+1, row+ud, col+lr, vital, visited))
-            
+        que.append((depth+1, row+ud, col+lr, vital))
